fedc49a678209cd7e2e9c986888764d03969de81
Md Arif <arif.20527@sscbs.du.ac.in>
2025-04-07 01:55:06 +0530

    user details api route completed

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,14 +4,12 @@ import Student from "../models/student.model.js";
 import Batch from "../models/batch.model.js";
 import { generateAccessToken, generateRefreshToken } from "../utils/tokens.js";
 
-
-
-export  function mapToken(user){
+export function mapToken(user) {
   return generateAccessToken({
     id: user._id,
     rollNumber: user.rollNumber,
     role: user.role,
-  })
+  });
 }
 export async function userLogin(req, res) {
   const result = validationResult(req);
@@ -21,7 +19,7 @@ export async function userLogin(req, res) {
       const student = await Student.findOne({ rollNumber: data.rollNumber });
       if (student) {
         if (student.comparePassword(data.password)) {
-          const token = mapToken(student)
+          const token = mapToken(student);
           const refreshToken = generateRefreshToken({
             id: student._id,
           });
@@ -32,20 +30,20 @@ export async function userLogin(req, res) {
           return res
             .status(200)
             .cookie("token", token, options)
-            .cookie("refreshToken",refreshToken,options)
+            .cookie("refreshToken", refreshToken, options)
             .json("logged in");
         } else {
-          return res.status(400).send({ messge: "Wrong Password" });
+          return res.status(400).send({ message: "Wrong Password" });
         }
       } else {
-        return res.status(400).send({ messge: "Invalid details" });
+        return res.status(400).send({ message: "Invalid details" });
       }
     } catch (error) {
       console.error("Error Occured at userLogin ", error);
       return res.status(400).json({ message: "Some Error Occured" });
     }
   } else {
-    res.status(400).send({ messge: "Invalid details provided" });
+    res.status(400).send({ message: "Invalid details provided" });
   }
 }
 
@@ -75,4 +73,39 @@ export async function userBatch(re, res) {
     session: "2023-2024",
   });
   res.send("created");
+}
+
+export async function getUserDetails(req, res, next) {
+  const user = await Student.findById(req.id, "-password -refreshToken -role")
+    .populate("person")
+    .populate("batch");
+  res.status(200).json({ data: user });
+}
+export async function setUserDetails(req, res) {
+  const result = validationResult(req);
+  if (result.isEmpty()) {
+    const data = matchedData(req);
+    if (data.studentid == req.id) {
+      const person = data.person;
+      const student = data.student;
+      try {
+        await Person.findByIdAndUpdate({ _id: data.personid }, person,{runValidators:true});
+        await Student.findByIdAndUpdate({ _id: data.studentid }, student,{runValidators:true});
+        return res.status(200).json({ message: "details updated successflly" });
+      } catch (error) {
+        return res.status(400).json({
+          message: "Some error occured updating information",
+          error: error,
+        });
+      }
+    } else {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized Access to Change User Information" });
+    }
+  } else {
+    return res.status(400).json({ messsage: "Invalid details provided" });
+  }
+
+  res.json(req.body);
 }
